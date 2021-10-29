762d56146d43b7699ef8e2fcf8b125fb6db79272
agamezcristian <cristian.agamez17@gmail.com>
2021-10-28 23:14:14 -0500

    segundo commit
    se remueven archivos no requeridos

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,7 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
+  },  
   {
     path: '/list',
     name: 'List',
